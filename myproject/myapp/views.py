a09ea4799e9c73f64fab46a6b574a136d993ab49
Administrator <administrator@AdministorsiMac.home>
2024-04-25 17:34:48 -0400

    Add UpdateView func

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import redirect
 from .forms import TodoForm
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
 
 
 class TaskListView(ListView):
@@ -16,6 +18,17 @@ class TaskDetailView(DetailView):
     model = Task
     template_name = 'myapp/detail.html'
     context_object_name = 'task'
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'myapp/update.html'
+    context_object_name = 'task'
+    fields = ['name', 'priority', 'date']
+    success_url = '/'
+
+    def get_success_url(self):
+        return reverse_lazy('cbvdetail', kwargs={'pk': self.object.id})
 
 
 def index(request):
