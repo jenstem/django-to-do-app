84d2ea26aff386e6eac7262d4d58db127dac917b
Administrator <administrator@AdministorsiMac.home>
2024-04-25 14:00:15 -0400

    Include add func in index.html  + remove add.html

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,7 +3,8 @@ from .models import Task
 from django.shortcuts import redirect
 
 
-def add(request):
+def index(request):
+    task_list = Task.objects.all()
     if request.method == 'POST':
         name = request.POST.get('name', '')
         priority = request.POST.get('priority', '')
@@ -11,10 +12,4 @@ def add(request):
         task = Task(name=name, priority=priority)
         task.save()
         return redirect('/')
-
-    return render(request, 'myapp/add.html')
-
-
-def index(request):
-    task_list = Task.objects.all()
     return render(request, 'myapp/index.html', {'task_list': task_list})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -20,6 +20,5 @@ from myapp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('add/', views.add, name='add'),
     path('', views.index, name='index'),
 ]
