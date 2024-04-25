a47736a8b08a83d740ff4c6679314162a55207a6
Administrator <administrator@AdministorsiMac.home>
2024-04-25 10:53:20 -0400

    Add post handling to Task model

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Task
 
 
 def add(request):
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        priority = request.POST.get('priority', '')
+
+        task = Task(name=name, priority=priority)
+        task.save()
+
     return render(request, 'myapp/add.html')
