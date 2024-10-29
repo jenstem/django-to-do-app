1087fcc1efacc9787a6bcc495d0fd6aac7529714
Jen <124745888+jenstem@users.noreply.github.com>
2024-10-29 17:34:03 -0400

    Add docstring to class + function

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,7 +2,21 @@ from django.db import models
 import datetime
 
 class Task(models.Model):
+    """
+    Represents a task in the application.
+
+    Attributes:
+        name (str): The name of the task.
+        priority (int): The priority level of the task.
+        date (date): The date associated with the task, defaulting to today.
+    """
     def __str__(self):
+        """
+        Returns the string representation of the Task instance.
+
+        Returns:
+            str: The name of the task.
+        """
         return self.name
 
     name = models.CharField(max_length=100)
