b1d85cc8fbc88e163a1aca67fea6ea6a79f9dcd2
Jen <124745888+jenstem@users.noreply.github.com>
2024-10-29 17:35:37 -0400

    Add docstring

--- myproject/myapp/apps.py
+++ myproject/myapp/apps.py
@@ -2,5 +2,12 @@ from django.apps import AppConfig
 
 
 class MyappConfig(AppConfig):
+    """
+    Configuration class for the 'myapp' application.
+
+    Attributes:
+        default_auto_field (str): The default auto field type for models.
+        name (str): The name of the application.
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "myapp"
