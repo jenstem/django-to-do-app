7bc80f4ad7d3f5cf279befa41a33287d3faa67ad
Jen <124745888+jenstem@users.noreply.github.com>
2024-10-29 17:34:46 -0400

    Add docstring

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -2,6 +2,11 @@ from .models import Task
 from django import forms
 
 class TodoForm(forms.ModelForm):
+    """
+    A form for creating and updating Task instances.
+
+    Meta class defines the model and fields to be included in the form.
+    """
     class Meta:
         model = Task
         fields = ['name', 'priority', 'date']
